0b8d9f7aa8699cd17af94574e6aa1a65ead4c1a0
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-09-06 12:05:13 +0300

    left-bar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,12 +3,20 @@ import React from 'react'
 function Sidebar() {
   return (
     <React.Fragment>
-      <div className="logo">
-        <a>Brand<b>Colors</b></a>
-      </div>
-      <div className="description">
-        The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
-      </div>
+      <aside className="sidebar">
+        <div className="logo">
+          <a>Brand<b>Colors</b></a>
+        </div>
+        <div className="description">
+          The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+        </div>
+        <nav className="menu">
+          <ul>
+            <li><a>Suggest a Brand</a></li>
+            <li><a>About BrandColors</a></li>
+          </ul>
+        </nav>
+      </aside>
     </React.Fragment>
   )
 }
