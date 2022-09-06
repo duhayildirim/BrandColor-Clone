30e0dd41f6a26e00eb7a7b950b4ba2edadc3e3ff
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-09-06 11:15:04 +0300

    create react app

---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <React.Fragment>
+      <div className="logo">
+        <a>Brand<b>Colors</b></a>
+      </div>
+      <div className="description">
+        The biggest collection of official brand color codes around. Curated by @brandcolors and friends.
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Sidebar
