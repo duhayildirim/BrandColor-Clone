0e1133217e1c78553ebcdb92b5c7dec3c1c6a4ba
Duha YILDIRIM <1duhayildirim@gmail.com>
2022-09-07 11:56:03 +0300

    data list

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { getContrastYIQ } from '../Contrast'
 
 function Brand({ brand }) {
-  console.log(brand)
+
   return (
-    <div>
-      Brand
+    <div className="brand">
+      <h5> {brand.title} </h5>
+      <div className="brand-colors">
+        {brand.colors.map((color, key) => (
+          <span key={key} style={{ '--bgColor': `#${color}`, '--textColor': `${getContrastYIQ(color)}` }}>{color}</span>
+        ))}
+      </div>
     </div>
   )
 }
